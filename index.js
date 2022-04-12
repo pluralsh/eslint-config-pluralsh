281307cc6530f27cf16fa88a2ad9c5a800cf86f5
David Hérault <dherault@gmail.com>
2022-04-13 00:00:42 +0300

    v3.0.0

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
 module.exports = {
-  parser: '@babel/eslint-parser',
-  parserOptions: {
-    requireConfigFile: false,
-    babelOptions: {
-      presets: ['@babel/preset-env', '@babel/preset-react'],
-    },
-  },
   extends: ['airbnb', 'airbnb/hooks'],
   env: {
     browser: true,
